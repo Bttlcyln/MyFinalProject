75cb9248112521118b49df1deda35522580e3d99
Bttlcyln <battalcyln@gmail.com>
2024-04-07 13:08:42 +0300

    07.04.2024

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,18 +1,30 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Conctere.EntityFramework;
 
 var builder = WebApplication.CreateBuilder(args);
+//
+Host.CreateDefaultBuilder(args);
+
+
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddSingleton<IProductService, ProductManager>();
-builder.Services.AddSingleton<IProductDal, EfProductDal>();
+//builder.Services.AddSingleton<IProductService, ProductManager>();
+//builder.Services.AddSingleton<IProductDal, EfProductDal>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+//
+builder.Host.UseServiceProviderFactory(services => new AutofacServiceProviderFactory()).ConfigureContainer<ContainerBuilder>(builder =>
+{
+    builder.RegisterModule(new AutofacBusinessModule());
+});
+//
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
